ddba17752a779324c91e2fe41df2a82de73e7849
DiasIshbulatov10 <dias.ishbulatov@gmail.com>
2024-04-10 21:50:52 +0600

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,13 @@ function App() {
     <div className='w-full h-screen flex items-center justify-center flex-col bg-black text-white font-sans'>
       <h1>Token Pair Rate App</h1>
       <p>This is a simple app built with React and Web3Modal.</p>
+
       <w3m-button />
+
       <div className="mt-3">
         <w3m-network-button />
       </div>
+      
       <div className="mt-5">
         <Input value={tokenA} onChange={e => setTokenA(e.target.value)}></Input>
         <Input value={tokenB} onChange={e => setTokenB(e.target.value)} className="mt-3"></Input> 
